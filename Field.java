d813d2e523c8171930f6d46480d0c2b973ba389e
Nicola Castellani <caste.92@hotmail.it>
2013-02-27 15:49:41 -0800

    Aggiunta classe costruttore, implementate librerie e setting del metodo di chiusura della finestra

--- Field.java
+++ Field.java
@@ -1,5 +1,15 @@
 package it.univr.GameOfLife;
+import java.awt.Color;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
-public class Field {
-
+public class Field extends JFrame{
+	public Field(){
+		super("The Game Of Life");
+		actionOnExit();
+	}
+	public void actionOnExit(){
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 }
