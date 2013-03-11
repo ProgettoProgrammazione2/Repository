65332df2838ea093da262d5265d54ebd62bafa82
PietroMsn <pietro.musoni@gmail.com>
2013-03-11 19:31:22 +0100

    Perfezionamento della finestra
    ora quando si deseleziona un pulsante torna all'icona iniziale.

--- SelectionWindow.java
+++ SelectionWindow.java
@@ -13,15 +13,18 @@ import javax.swing.JButton;
 
 
 
-class SelectionWindow extends JFrame implements ActionListener {
-  public int WhichFigure;
-	public boolean SetFigure;
+public class SelectionWindow extends JFrame implements ActionListener {
+	
+	static final long serialVersionUID = 2000;
+	public static int WhichFigure;
+	public static boolean SetFigure;
 	JPanel topPanel;
-	public JButton bottone1, bottone2, bottone3, bottone4, bottone5, bottone6, bottone7, bottone8, bottone9, bottone10;
+	public JButton bottone1, bottone2, bottone3, bottone4, bottone5, bottone6, bottone7, bottone8, bottone9;
+	
 	public SelectionWindow() {
 		setTitle( "Finestra di selezione" );				//creazione della finestra di selezione
-		setSize( 85 * 3 + 20, 85 * 4 + 50); 					//della figura seme per game of life
-		topPanel = new JPanel(new GridLayout(4,3));
+		setSize( 85 * 3 + 20, 85 * 3 + 50); 					//della figura seme per game of life
+		topPanel = new JPanel(new GridLayout(3,3));
 		
 		bottone1 = new JButton(new ImageIcon("images/bottone1.gif"));		//associa ad ogni bottone 
 		bottone2 = new JButton(new ImageIcon("images/bottone2.gif"));			//un'immagine seme per 
@@ -31,9 +34,8 @@ class SelectionWindow extends JFrame implements ActionListener {
 		bottone6 = new JButton(new ImageIcon("images/bottone6.gif"));
 		bottone7 = new JButton(new ImageIcon("images/bottone7.gif"));
 		bottone8 = new JButton(new ImageIcon("images/bottone8.gif"));
-		bottone9 = new JButton(new ImageIcon("images/bottone6.gif"));
-		bottone10 = new JButton(new ImageIcon("images/bottone10.gif"));
-				
+		bottone9 = new JButton(new ImageIcon("images/bottone9.gif"));
+						
 		topPanel.add(bottone1);
 		topPanel.add(bottone2);
 		topPanel.add(bottone3);
@@ -43,9 +45,9 @@ class SelectionWindow extends JFrame implements ActionListener {
 		topPanel.add(bottone7);
 		topPanel.add(bottone8);
 		topPanel.add(bottone9);
-		topPanel.add(bottone10);
-		
-		getContentPane().add(topPanel);			
+				
+		getContentPane().add(topPanel);
+		setResizable(false);
 		Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
 		Dimension frame = getSize();
 		setLocation(
@@ -58,88 +60,182 @@ class SelectionWindow extends JFrame implements ActionListener {
 																	
 		bottone1.addActionListener( new ActionListener() {		//ad ogni bottone si passa una 
 			public void actionPerformed(ActionEvent e) {		//variabile per determinare
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone1.setIcon(new ImageIcon("images/negative1.gif"));
-				SetFigure = true;									//a quale figura ci si riferisce
-				WhichFigure = 1;
+				if (!SetFigure) {
+					bottone1.setIcon(new ImageIcon("images/negative1.gif"));
+					WhichFigure = 1;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 1){
+						ChangeIcon(1);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone1.setIcon(new ImageIcon("images/negative1.gif"));
+						WhichFigure = 1;
+					}
 				
 			}
 		});
 		bottone2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {          
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone2.setIcon(new ImageIcon("images/negative2.gif"));
-				SetFigure = true;
-				WhichFigure = 2;
-			}
+			public void actionPerformed(ActionEvent e) { 
+				if (!SetFigure) {
+					bottone2.setIcon(new ImageIcon("images/negative2.gif"));
+					WhichFigure = 2;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 2) {
+						ChangeIcon(2);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone2.setIcon(new ImageIcon("images/negative2.gif"));
+						WhichFigure = 2;
+					}				
+				}
 		});
 		bottone3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone3.setIcon(new ImageIcon("images/negative3.gif"));
-				SetFigure = true;
-				WhichFigure = 3;
+				if (!SetFigure) {
+					bottone3.setIcon(new ImageIcon("images/negative3.gif"));
+					WhichFigure = 3;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 3){
+						ChangeIcon(3);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone3.setIcon(new ImageIcon("images/negative3.gif"));
+						WhichFigure = 3;
+					}		
 			}
 		});
 		bottone4.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone4.setIcon(new ImageIcon("images/negative4.gif"));
-				SetFigure = true;
-				WhichFigure = 4;
+				if (!SetFigure) {
+					bottone4.setIcon(new ImageIcon("images/negative4.gif"));
+					WhichFigure = 4;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 4){
+						ChangeIcon(4);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone4.setIcon(new ImageIcon("images/negative4.gif"));
+						WhichFigure = 4;
+					}		
 			}
 		});
 		bottone5.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone5.setIcon(new ImageIcon("images/negative5.gif"));
-				SetFigure = true;
-				WhichFigure = 5;
+				if (!SetFigure) {
+					bottone5.setIcon(new ImageIcon("images/negative5.gif"));
+					WhichFigure = 5;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 5){
+						ChangeIcon(5);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone5.setIcon(new ImageIcon("images/negative5.gif"));
+						WhichFigure = 5;
+					}		
 			}
 		});
+		
 		bottone6.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone6.setIcon(new ImageIcon("images/negative6.gif"));
-				SetFigure = true;
-				WhichFigure = 6;
+				if (!SetFigure) {
+					bottone6.setIcon(new ImageIcon("images/negative6.gif"));
+					WhichFigure = 6;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 6){
+						ChangeIcon(6);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone6.setIcon(new ImageIcon("images/negative6.gif"));
+						WhichFigure = 6;
+					}		
 			}
 		});
+		
 		bottone7.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone7.setIcon(new ImageIcon("images/negative7.gif"));
-				SetFigure = true;
-				WhichFigure = 7;
+				if (!SetFigure) {
+					bottone7.setIcon(new ImageIcon("images/negative7.gif"));
+					WhichFigure = 7;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 7){
+						ChangeIcon(7);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone7.setIcon(new ImageIcon("images/negative7.gif"));
+						WhichFigure = 7;
+					}		
 			}
 		});
+		
 		bottone8.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone8.setIcon(new ImageIcon("images/negative8.gif"));
-				SetFigure = true;
-				WhichFigure = 8;
+				if (!SetFigure) {
+					bottone8.setIcon(new ImageIcon("images/negative8.gif"));
+					WhichFigure = 8;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 8){
+						ChangeIcon(8);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone8.setIcon(new ImageIcon("images/negative8.gif"));
+						WhichFigure = 8;
+					}		
 			}
 		});
+		
 		bottone9.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(SetFigure)
-					ChangeIcon(WhichFigure);
-				bottone9.setIcon(new ImageIcon("images/negative9.gif"));
-				SetFigure = true;
-				WhichFigure = 9;
+				if (!SetFigure) {
+					bottone9.setIcon(new ImageIcon("images/negative9.gif"));
+					WhichFigure = 9;
+					SetFigure = true;
+				}
+				else
+					if (WhichFigure == 9){
+						ChangeIcon(9);
+						SetFigure = false;
+					}
+					else {
+						ChangeIcon(WhichFigure);
+						bottone9.setIcon(new ImageIcon("images/negative9.gif"));
+						WhichFigure = 9;
+					}		
 			}
 		});
 		
-		
+				
 	}
 	
 	
@@ -147,9 +243,9 @@ class SelectionWindow extends JFrame implements ActionListener {
 	
 	public void ChangeIcon(int WhichFigure) {
 		if (WhichFigure == 1)
-			bottone1.setIcon(new ImageIcon("images/bottone1.gif"));			//se si è già selezionato un bottone
-		else if(WhichFigure == 2)							//e lo si vuole cambiare ChangeIcon lo riporta
-			bottone2.setIcon(new ImageIcon("images/bottone2.gif"));			//alla forma originaria
+			bottone1.setIcon(new ImageIcon("images/bottone1.gif"));
+		else if(WhichFigure == 2)
+			bottone2.setIcon(new ImageIcon("images/bottone2.gif"));
 		else if(WhichFigure == 3)
 			bottone3.setIcon(new ImageIcon("images/bottone3.gif"));
 		else if(WhichFigure == 4)
@@ -164,7 +260,6 @@ class SelectionWindow extends JFrame implements ActionListener {
 			bottone8.setIcon(new ImageIcon("images/bottone8.gif"));
 		else if(WhichFigure == 9)
 			bottone9.setIcon(new ImageIcon("images/bottone9.gif"));
-		SetFigure = false;
 		}
 	
 	
@@ -172,6 +267,5 @@ class SelectionWindow extends JFrame implements ActionListener {
 																		//main per mostrare la finestra
 	public static void main(String args[]) {
 		new SelectionWindow().setVisible(true);
-		}
+	}
 }
-
